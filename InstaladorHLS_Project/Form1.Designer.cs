8ab680215bb9802c523c92dc5fd60bd2b8b13574
Carlos Suarez <carlos.suarez@redsalud.gov.cl>
2021-10-07 13:58:21 -0300

    Primer programilla

---
+++ InstaladorHLS_Project/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace InstaladorHLS_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSAM = new System.Windows.Forms.Button();
+            this.btnSALIR = new System.Windows.Forms.Button();
+            this.btnHojaDi = new System.Windows.Forms.Button();
+            this.btnODBC = new System.Windows.Forms.Button();
+            this.btnVNC = new System.Windows.Forms.Button();
+            this.btnTrazBio = new System.Windows.Forms.Button();
+            this.btnSAMHLS = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSAM
+            // 
+            this.btnSAM.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSAM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSAM.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSAM.Location = new System.Drawing.Point(21, 65);
+            this.btnSAM.Name = "btnSAM";
+            this.btnSAM.Size = new System.Drawing.Size(140, 47);
+            this.btnSAM.TabIndex = 0;
+            this.btnSAM.Text = "SAM";
+            this.btnSAM.UseVisualStyleBackColor = false;
+            // 
+            // btnSALIR
+            // 
+            this.btnSALIR.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSALIR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSALIR.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSALIR.Location = new System.Drawing.Point(287, 477);
+            this.btnSALIR.Name = "btnSALIR";
+            this.btnSALIR.Size = new System.Drawing.Size(140, 47);
+            this.btnSALIR.TabIndex = 1;
+            this.btnSALIR.Text = "SALIR";
+            this.btnSALIR.UseVisualStyleBackColor = false;
+            // 
+            // btnHojaDi
+            // 
+            this.btnHojaDi.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnHojaDi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHojaDi.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnHojaDi.Location = new System.Drawing.Point(548, 301);
+            this.btnHojaDi.Name = "btnHojaDi";
+            this.btnHojaDi.Size = new System.Drawing.Size(140, 47);
+            this.btnHojaDi.TabIndex = 2;
+            this.btnHojaDi.Text = "HOJA DIARIA";
+            this.btnHojaDi.UseVisualStyleBackColor = false;
+            // 
+            // btnODBC
+            // 
+            this.btnODBC.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnODBC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnODBC.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnODBC.Location = new System.Drawing.Point(548, 175);
+            this.btnODBC.Name = "btnODBC";
+            this.btnODBC.Size = new System.Drawing.Size(140, 47);
+            this.btnODBC.TabIndex = 3;
+            this.btnODBC.Text = "OBDC";
+            this.btnODBC.UseVisualStyleBackColor = false;
+            // 
+            // btnVNC
+            // 
+            this.btnVNC.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnVNC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVNC.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnVNC.Location = new System.Drawing.Point(548, 65);
+            this.btnVNC.Name = "btnVNC";
+            this.btnVNC.Size = new System.Drawing.Size(140, 47);
+            this.btnVNC.TabIndex = 4;
+            this.btnVNC.Text = "VNC";
+            this.btnVNC.UseVisualStyleBackColor = false;
+            // 
+            // btnTrazBio
+            // 
+            this.btnTrazBio.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnTrazBio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrazBio.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnTrazBio.Location = new System.Drawing.Point(21, 301);
+            this.btnTrazBio.Name = "btnTrazBio";
+            this.btnTrazBio.Size = new System.Drawing.Size(140, 47);
+            this.btnTrazBio.TabIndex = 5;
+            this.btnTrazBio.Text = "TRAZBIO";
+            this.btnTrazBio.UseVisualStyleBackColor = false;
+            // 
+            // btnSAMHLS
+            // 
+            this.btnSAMHLS.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSAMHLS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSAMHLS.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSAMHLS.Location = new System.Drawing.Point(21, 175);
+            this.btnSAMHLS.Name = "btnSAMHLS";
+            this.btnSAMHLS.Size = new System.Drawing.Size(140, 47);
+            this.btnSAMHLS.TabIndex = 6;
+            this.btnSAMHLS.Text = "SAM HLS";
+            this.btnSAMHLS.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::InstaladorHLS_Project.Properties.Resources.logo_vertical_HLS;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(728, 536);
+            this.Controls.Add(this.btnSAMHLS);
+            this.Controls.Add(this.btnTrazBio);
+            this.Controls.Add(this.btnVNC);
+            this.Controls.Add(this.btnODBC);
+            this.Controls.Add(this.btnHojaDi);
+            this.Controls.Add(this.btnSALIR);
+            this.Controls.Add(this.btnSAM);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSAM;
+        private System.Windows.Forms.Button btnSALIR;
+        private System.Windows.Forms.Button btnHojaDi;
+        private System.Windows.Forms.Button btnODBC;
+        private System.Windows.Forms.Button btnVNC;
+        private System.Windows.Forms.Button btnTrazBio;
+        private System.Windows.Forms.Button btnSAMHLS;
+    }
+}
+
