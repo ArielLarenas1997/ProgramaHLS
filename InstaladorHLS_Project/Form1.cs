986c828d4566cffa9e8be05ff2586a18e941df5b
Matias Tapia <mtapiar5@alumnos.ceduc.cl>
2021-10-11 12:53:47 -0300

    Diseño

--- InstaladorHLS_Project/Form1.cs
+++ InstaladorHLS_Project/Form1.cs
@@ -16,5 +16,72 @@ namespace InstaladorHLS_Project
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSALIR_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSAM_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+        // mover pantalla 
+        //creamos 3 variables tipo int
+        int m, mx, my;
+
+        private void TitleBar_MouseUp(object sender, MouseEventArgs e)
+        { 
+            // para dejar de mover el formulario m tiene que ser 0 
+
+            m = 0;
+
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            // m tendra valor de 1
+            m = 1;
+            //activa evento
+            mx = e.X;
+            // valor actual de la ubicacion del puntero
+            my = e.Y;
+
+
+        }
+
+        private void TitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            //mientras  se presiona el panel
+            if (m ==1)
+            { //codigo que permite mover el formulario
+                this.SetDesktopLocation(MousePosition.X - mx, MousePosition.Y - my);
+
+            }
+
+        }
     }
 }
