3514bd840731704f64c67faec8779998314d82de
Psicobyte <psicobyte@gmail.com>
2021-10-12 13:20:53 -0300

    codigo bat

--- InstaladorHLS_Project/Form1.Designer.cs
+++ InstaladorHLS_Project/Form1.Designer.cs
@@ -40,7 +40,7 @@
             // btnSAM
             // 
             this.btnSAM.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnSAM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSAM.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSAM.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnSAM.Location = new System.Drawing.Point(21, 65);
             this.btnSAM.Name = "btnSAM";
@@ -48,11 +48,12 @@
             this.btnSAM.TabIndex = 0;
             this.btnSAM.Text = "SAM";
             this.btnSAM.UseVisualStyleBackColor = false;
+            this.btnSAM.Click += new System.EventHandler(this.btnSAM_Click);
             // 
             // btnSALIR
             // 
             this.btnSALIR.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnSALIR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSALIR.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSALIR.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnSALIR.Location = new System.Drawing.Point(287, 477);
             this.btnSALIR.Name = "btnSALIR";
@@ -60,11 +61,12 @@
             this.btnSALIR.TabIndex = 1;
             this.btnSALIR.Text = "SALIR";
             this.btnSALIR.UseVisualStyleBackColor = false;
+            this.btnSALIR.Click += new System.EventHandler(this.btnSALIR_Click);
             // 
             // btnHojaDi
             // 
             this.btnHojaDi.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnHojaDi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHojaDi.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHojaDi.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnHojaDi.Location = new System.Drawing.Point(548, 301);
             this.btnHojaDi.Name = "btnHojaDi";
@@ -72,23 +74,25 @@
             this.btnHojaDi.TabIndex = 2;
             this.btnHojaDi.Text = "HOJA DIARIA";
             this.btnHojaDi.UseVisualStyleBackColor = false;
+            this.btnHojaDi.Click += new System.EventHandler(this.btnHojaDi_Click);
             // 
             // btnODBC
             // 
             this.btnODBC.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnODBC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnODBC.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnODBC.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnODBC.Location = new System.Drawing.Point(548, 175);
             this.btnODBC.Name = "btnODBC";
             this.btnODBC.Size = new System.Drawing.Size(140, 47);
             this.btnODBC.TabIndex = 3;
-            this.btnODBC.Text = "OBDC";
+            this.btnODBC.Text = "ODBC";
             this.btnODBC.UseVisualStyleBackColor = false;
+            this.btnODBC.Click += new System.EventHandler(this.btnODBC_Click);
             // 
             // btnVNC
             // 
             this.btnVNC.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnVNC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVNC.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnVNC.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnVNC.Location = new System.Drawing.Point(548, 65);
             this.btnVNC.Name = "btnVNC";
@@ -96,11 +100,12 @@
             this.btnVNC.TabIndex = 4;
             this.btnVNC.Text = "VNC";
             this.btnVNC.UseVisualStyleBackColor = false;
+            this.btnVNC.Click += new System.EventHandler(this.btnVNC_Click);
             // 
             // btnTrazBio
             // 
             this.btnTrazBio.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnTrazBio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrazBio.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnTrazBio.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnTrazBio.Location = new System.Drawing.Point(21, 301);
             this.btnTrazBio.Name = "btnTrazBio";
@@ -108,11 +113,12 @@
             this.btnTrazBio.TabIndex = 5;
             this.btnTrazBio.Text = "TRAZBIO";
             this.btnTrazBio.UseVisualStyleBackColor = false;
+            this.btnTrazBio.Click += new System.EventHandler(this.btnTrazBio_Click);
             // 
             // btnSAMHLS
             // 
             this.btnSAMHLS.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnSAMHLS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSAMHLS.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSAMHLS.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnSAMHLS.Location = new System.Drawing.Point(21, 175);
             this.btnSAMHLS.Name = "btnSAMHLS";
@@ -120,6 +126,7 @@
             this.btnSAMHLS.TabIndex = 6;
             this.btnSAMHLS.Text = "SAM HLS";
             this.btnSAMHLS.UseVisualStyleBackColor = false;
+            this.btnSAMHLS.Click += new System.EventHandler(this.btnSAMHLS_Click);
             // 
             // Form1
             // 
--- InstaladorHLS_Project/Form1.cs
+++ InstaladorHLS_Project/Form1.cs
@@ -16,5 +16,54 @@ namespace InstaladorHLS_Project
         {
             InitializeComponent();
         }
+
+        private void btnSALIR_Click(object sender, EventArgs e)
+        {
+            string message = "¿Quieres cerrar la aplicación?";
+            string title = "Cerrar aplicación.";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Error);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+                // Do something  
+            }
+            
+        }
+
+        private void btnSAM_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\BatsAdministrador\SAM.lnk");
+        }
+
+        private void btnSAMHLS_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\BatsAdministrador\SAMHLS.lnk");
+        }
+
+        private void btnTrazBio_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\BatsAdministrador\Trazbio.lnk");
+        }
+
+        private void btnVNC_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\BatsAdministrador\VNC.lnk");
+        }
+
+        private void btnODBC_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\BatsAdministrador\ODBC-INFORMIX-SDK.4.10.lnk");
+        }
+
+        private void btnHojaDi_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\BatsAdministrador\HojaDiaria.lnk");
+        }
+
+        
     }
 }
